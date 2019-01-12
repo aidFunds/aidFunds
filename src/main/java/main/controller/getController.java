a50b62af84b94aa444466548db67baed9100312c
gaurameh <gauravmehta.cs@gmail.com>
2019-01-12 11:45:51 +0530

    created new Organization

--- src/main/java/main/controller/getController.java
+++ src/main/java/main/controller/getController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class getController {
     @RequestMapping("/")
     public String getMessage() {
-        return "Greetings from aidFunds!";
+        return "Greetings from aidFunds Organization!";
     }
 }
