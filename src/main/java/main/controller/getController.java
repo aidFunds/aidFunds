8d165c05999f945cdd61ecfe79e7e5857e30d835
gaurameh <gauravmehta.cs@gmail.com>
2019-01-12 11:48:25 +0530

    new world

--- src/main/java/main/controller/getController.java
+++ src/main/java/main/controller/getController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class getController {
     @RequestMapping("/")
     public String getMessage() {
-        return "Greetings from aidFunds Organization!";
+        return "Greetings from aidFunds Organization! new world begins";
     }
 }
